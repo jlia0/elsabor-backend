d36002442eaf63c4d45d340e1adcb0484223119a
JLiao98 <jian.liao1@ucalgary.ca>
2020-04-18 23:54:39 -0600

    Added CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors_proxy = require('cors-anywhere');
 // const mqtt = require('mqtt');
 // const mongoose = require('mongoose');
 // const actions = require('./src/actions/actions');
@@ -30,15 +31,30 @@ app.use('/users', userRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(createError(404)));
 
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-};
+// Listen on a specific host via the HOST environment variable
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+const port = process.env.PORT || 8080;
 
-app.use(allowCrossDomain);
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2'],
+  })
+  .listen(port, host, function() {
+    console.log(`Running CORS Anywhere on ${host}:${port}`);
+  });
+
+// const allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// };
+//
+// app.use(allowCrossDomain);
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   res.header('Cache-Control', 'no-cache');
